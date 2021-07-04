1c4fcd8d35fef4f8db1adca7ac62bee3fd6296a9
Jialiang Han <hanjiali@umich.edu>
2021-07-04 23:45:46 +0800

    add visualization node , but in rviz still no shape there.

--- src/agent.cpp
+++ src/agent.cpp
@@ -5,6 +5,7 @@
 #include "ros/ros.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "visualization_msgs/Marker.h"
 
 int main(int argc,char **argv){
 
@@ -27,10 +28,30 @@ int main(int argc,char **argv){
     current_position.pose.position.x = initial_position_x; // unit is meter
     current_position.pose.position.y = initial_position_y; //unit is meter
     current_position.pose.position.z = initial_position_theta; //unit is degree
-    
+
+    // ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker",1);   
+    // uint32_t shape = visualization_msgs::Marker::CYLINDER;
+
     ros::Rate loop_rate(0.5);
 
     while (ros::ok()){
+        // visualization_msgs::Marker marker;
+        // marker.header.frame_id = "marker";
+        // marker.header.stamp = ros::Time::now();
+        // marker.ns = "marker";
+        // marker.id = 0;
+        // marker.type = shape;
+        // marker.action = visualization_msgs::Marker::ADD;
+        // marker.pose = current_position.pose; 
+        // marker.scale.x = 1;
+        // marker.scale.y = 1;
+        // marker.scale.z = 1;
+        // marker.color.r = 0.0f;
+        // marker.color.g = 1.0f;
+        // marker.color.b = 0.0f;
+        // marker.color.a = 1.0; 
+        // marker.lifetime = ros::Duration(5);
+        // marker_pub.publish(marker);
         pub.publish(current_position);
         ros::spinOnce();
         loop_rate.sleep();
--- src/tf_broadcaster.cpp
+++ src/tf_broadcaster.cpp
@@ -2,4 +2,24 @@
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/OccupancyGrid.h>
 
-void map_callback
+
+nav_msgs::OccupancyGridPtr grid;
+
+void map_callback(const nav_msgs::OccupancyGrid::Ptr map){
+    grid = map;
+}
+
+int main(int argc, char** argv){
+    ros::init(argc,argv,"tf_broadcaster");
+    ros::NodeHandle n;
+
+    ros::Subscriber map = n.subscribe("grid_map", 1, map_callback);
+    tf::Pose tfPose;
+
+    ros::Rate loop_rate(0.5);
+    tf::TransformBroadcaster broadcaster;
+
+    while (ros::ok()){
+        
+    }
+}
