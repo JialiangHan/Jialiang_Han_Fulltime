ace2af82e08515e3edc89ccefd8407ad9d700272
Jialiang Han <hanjiali@umich.edu>
2021-07-14 16:26:47 +0800

    lower the publish rate

--- src/agent.cpp
+++ src/agent.cpp
@@ -45,7 +45,7 @@ int main(int argc,char **argv){
     marker.color.b = 0.0f;
     marker.color.a = 1.0; 
 
-    ros::Rate loop_rate(30);
+    ros::Rate loop_rate(10);
 
     while (ros::ok()){
         marker_pub.publish(marker);
