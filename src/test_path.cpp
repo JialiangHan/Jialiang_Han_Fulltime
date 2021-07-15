4cddf2b1985f41bf7e7f9b5c9d865a9e5ff6f580
Jialiang Han <hanjiali@umich.edu>
2021-07-15 20:34:08 +0800

    test_path.cpp works: partially passed test for path.cpp. path.publishPath not work. delete this function in path.cpp

---
+++ src/test_path.cpp
@@ -0,0 +1,54 @@
+#include <ros/ros.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+
+#include "astar.h"
+#include "path.h"
+
+using namespace planner;
+
+int main(int argc, char **argv){
+
+    ros::init(argc, argv, "test_path");
+
+    ros::NodeHandle n;
+    // test path.cpp using results from astar.cpp
+    int width =10;
+    int height =10;
+    Node3D* nodes3D = new Node3D[width*height]();
+    // retrieving goal position
+    int x =2 ;
+    int y = 0 ;
+    const Node3D nGoal(x, y, 0, 0, 0, nullptr);
+    // retrieving start position
+    x = 0 ;
+    y = 8 ;
+    Node3D nStart(x, y, 0, 0, 0, nullptr);
+    // CLEAR THE PATH
+    Astar astar;
+    // find goal node
+    Node3D* solution = astar.path_planner(nStart,nGoal,nodes3D, width,height);
+    // trace its parent and put it into a path list(vector)
+    astar.trace_path(solution);
+    vector<Node3D> path;
+    path = astar.get_path();
+    delete [] nodes3D;
+    Path transfer;
+    transfer.clear();
+    transfer.update_path(path);
+
+    // transfer.publishPath();
+    
+    nav_msgs::Path nav_path;
+    nav_path = transfer.get_path();
+    ros::Publisher path_pub = n.advertise<nav_msgs::Path>("path",1);
+
+    while(ros::ok()){
+        path_pub.publish(nav_path);
+        // path_pub.publish(pl.get_path());
+        ros::spinOnce();
+    }
+    // this function is a must or spinOnce;
+    ros::spin();
+    return 0;
+}
