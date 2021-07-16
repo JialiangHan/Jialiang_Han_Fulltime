480f8822ac43351c54b521470fbe81fd44b73f62
Jialiang Han <hanjiali@umich.edu>
2021-07-16 14:15:15 +0800

    delete one comment

--- src/get_plan_client.cpp
+++ src/get_plan_client.cpp
@@ -15,7 +15,6 @@ int main(int argc, char **argv)
   ros::Publisher path_pub = n.advertise<nav_msgs::Path>("path",1);
   client.call(srv);
   ros::Rate loop_rate(1);
-  // ros::service::call("get_plan",srv);
   nav_msgs::Path path = srv.response.path;
   while (ros::ok()){
     path_pub.publish(path);
