64d191cc860ffb83b17b551695dddc0d724af446
Jialiang Han <hanjiali@umich.edu>
2021-07-17 09:52:30 +0800

    set publish topic a private toptic

--- src/update_goal_client.cpp
+++ src/update_goal_client.cpp
@@ -6,16 +6,19 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "update_goal_client");
 
   ros::NodeHandle n;
+  ros::NodeHandle nh("~");
   ros::ServiceClient client = n.serviceClient<jialiang_han_fulltime::UpdateGoal>("update_goal");
   jialiang_han_fulltime::UpdateGoal srv;
   srv.request.agent_name = argv[1];
   srv.request.goal.pose.position.x = atoi(argv[2]);
   srv.request.goal.pose.position.y = atoi(argv[3]);
   srv.request.goal.pose.position.z = atoi(argv[4]);
-  ros::Publisher path_pub = n.advertise<nav_msgs::Path>("path",1);
+  ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("path",1);
+  // call service not work
   client.call(srv);
   ros::Rate loop_rate(1);
-  nav_msgs::Path path = srv.response.path;
+  nav_msgs::Path path;
+  path = srv.response.path;
   while (ros::ok()){
     path_pub.publish(path);
     ros::spinOnce();
